96ab51ced8e8fce7aea81b9c9f971444129a61c3
Adriel Diaz <adriexnet@gmail.com>
2021-06-05 04:41:40 -0600

    feat(hapi): add new env variables to execute contract actions

---
+++ hapi/src/config/affiliate.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  account: process.env.HAPI_AFFILIATE_ACCOUNT,
+  password: process.env.HAPI_AFFILIATE_PASSWORD
+}
--- hapi/src/config/index.js
+++ hapi/src/config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
+  affiliateConfig: require('./affiliate.config'),
   networkConfig: require('./network.config'),
   hasuraConfig: require('./hasura.config'),
   serverConfig: require('./server.config')
