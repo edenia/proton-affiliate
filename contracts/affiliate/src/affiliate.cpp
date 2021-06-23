08f8dbbd92392bc992edab499adb0dbd587a089e
Adriel Diaz <adriexnet@gmail.com>
2021-06-23 00:24:45 -0600

    fix(contracts): typos in errors messages

--- contracts/affiliate/src/affiliate.cpp
+++ contracts/affiliate/src/affiliate.cpp
@@ -17,7 +17,7 @@ ACTION affiliate::addadmin(name admin) {
 
   auto admin_itr = _users.find(admin.value);
   check(admin_itr == _users.end(), admin.to_string() + " account is already an affiliate");
-  check(has_valid_kyc(admin), "userinfo for " + admin.to_string() + " it's not verified");
+  check(has_valid_kyc(admin), "KYC for " + admin.to_string() + " is not verified");
 
   if (admin_itr == _users.end()) {
     _users.emplace(get_self(), [&](auto& usr) {
@@ -46,7 +46,7 @@ ACTION affiliate::adduser(name admin, name user, uint8_t role) {
   check(admin != user, "cannot add self as user");
   check(is_account(user), user.to_string() + " account does not exist");
   check(role != user_roles::ADMIN, "role not allowed");
-  check(has_valid_kyc(user), "userinfo for " + user.to_string() + " it's not verified");
+  check(has_valid_kyc(user), "KYC for " + user.to_string() + " is not verified");
   
   users_table _users(get_self(), get_self().value);
   auto admin_itr = _users.find(admin.value);
@@ -132,7 +132,7 @@ ACTION affiliate::verifykyc(name invitee) {
   check(_referral != _referrals.end(), "referral with invitee " + invitee.to_string() + " does not exist");
   check(_referral->status == referral_status::PENDING_KYC_VERIFICATION, "invalid status for invitee " + invitee.to_string() + " referral");
   check(_referral->expires_on > current_time_point(), "referral already expired for invitee " + invitee.to_string());
-  check(has_valid_kyc(invitee), "userinfo for " + invitee.to_string() + " it's not verified");
+  check(has_valid_kyc(invitee), "KYC for " + invitee.to_string() + " is not verified");
 
   _referrals.modify(_referral, get_self(), [&](auto& row) {
     row.status = referral_status::PENDING_PAYMENT;
