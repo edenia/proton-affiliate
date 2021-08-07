c5d547c7f828062781eb8bb84e6229666a5e02d3
Xavier Fernandez <xavier@crservers.com>
2021-08-07 17:50:51 -0600

    fix(webapp): add terms page for ricardian contracts

--- webapp/src/routes/About/index.js
+++ webapp/src/routes/About/index.js
@@ -4,8 +4,11 @@ import { makeStyles } from '@material-ui/styles'
 import { useTranslation } from 'react-i18next'
 import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
 
 import styles from './styles'
+import Logo from './logo'
 
 const useStyles = makeStyles(styles)
 
@@ -15,15 +18,48 @@ const About = () => {
 
   return (
     <Box>
-      <Grid container direction="column">
-        <Grid item xs>
-          <Grid container direction="column">
-            <Typography variant="h3" className={classes.title}>
-              {t('title')}
-            </Typography>
+      <Card>
+        <CardContent>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Box>
+                <Box className={classes.boxInfo}>
+                  <Typography variant="body2" align="justify" paragraph>
+                    {t('body.paragraph1')}
+                  </Typography>
+                </Box>
+
+                <Box className={classes.boxInfo}>
+                  <Typography variant="h4">{t('subtitle1')}</Typography>
+                  <Typography variant="body2" align="justify" paragraph>
+                    {t('body1.paragraph1')}
+                  </Typography>
+                </Box>
+
+                <Box className={classes.boxInfo}>
+                  <Typography variant="h4">{t('subtitle2')}</Typography>
+                  <Typography variant="body2" align="justify" paragraph>
+                    {t('body2.paragraph1')}
+                  </Typography>
+                </Box>
+
+                <Box className={classes.boxInfo}>
+                  <Typography variant="h4">{t('subtitle3')}</Typography>
+                  <Typography variant="body2" align="justify" paragraph>
+                    {t('body3.paragraph1')}
+                  </Typography>
+                  <Typography variant="body2" align="justify" paragraph>
+                    {t('body3.paragraph2')}
+                  </Typography>
+                </Box>
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={5}>
+              <Logo className={classes.logo} />
+            </Grid>
           </Grid>
-        </Grid>
-      </Grid>
+        </CardContent>
+      </Card>
     </Box>
   )
 }
--- webapp/src/routes/Help/index.js
+++ webapp/src/routes/Help/index.js
@@ -2,7 +2,14 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/styles'
 import { useTranslation } from 'react-i18next'
+import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
+import Link from '@material-ui/core/Link'
+import HttpIcon from '@material-ui/icons/Http'
+import TelegramIcon from '@material-ui/icons/Telegram'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
 
 import styles from './styles'
 
@@ -14,9 +21,57 @@ const Help = () => {
 
   return (
     <Box>
-      <Typography variant="h3" className={classes.title}>
-        {t('title')}
-      </Typography>
+      <Card>
+        <CardContent>
+          <Grid container direction="column">
+            <Grid item xs>
+              <Grid container direction="column">
+                <Typography variant="h4" className={classes.title}>
+                  {t('title')}
+                </Typography>
+                <Typography variant="body2" align="justify" paragraph>
+                  {t('paragraph')}
+                </Typography>
+
+                <Box className={classes.boxLinks}>
+                  <GitHubIcon />
+                  <Link
+                    href="https://github.com/eoscostarica/proton-affiliate"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Typography variant="body1">{t('protonGitHub')}</Typography>
+                  </Link>
+                </Box>
+                <Box className={classes.boxLinks}>
+                  <TelegramIcon />
+                  <Link
+                    href="https://t.me/protonxpr"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Typography variant="body1">
+                      {t('telegramChannel')}
+                    </Typography>
+                  </Link>
+                </Box>
+                <Box className={classes.boxLinks}>
+                  <HttpIcon />
+                  <Link
+                    href="https://www.protonchain.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <Typography variant="body1">
+                      {t('protonWebsite')}
+                    </Typography>
+                  </Link>
+                </Box>
+              </Grid>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
     </Box>
   )
 }
