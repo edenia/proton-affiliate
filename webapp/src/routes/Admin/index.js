8f25e585d151b1be0e3914c641f4732523a5c756
Xavier Fernandez <xavier@crservers.com>
2021-08-09 21:19:50 -0600

    feat(webapp): re-order admin page components

--- webapp/src/routes/Admin/index.js
+++ webapp/src/routes/Admin/index.js
@@ -351,6 +351,20 @@ const Admin = () => {
         <Typography className={classes.adminTitle}>{t('title')}</Typography>
         <Typography className={classes.adminInfo}>{t('pageInfo')}</Typography>
       </Box>
+      <Accordion title="Referral Payments">
+        <TableSearch
+          tableName="payment"
+          onSelectItem={handleOnSelectItem}
+          selected={selected.payment || []}
+          useLoadMore
+          rows={referralRows}
+          showColumnCheck={false}
+          headCells={headCellReferralPayment}
+          handleOnLoadMore={handleOnLoadMoreReferrals}
+          onClickRow={handleOnClickReferral}
+          idName="invitee"
+        />
+      </Accordion>
       <Accordion title="New Affiliates">
         <TableSearch
           tableName="new"
@@ -377,20 +391,6 @@ const Admin = () => {
           headCells={headCellUserApprovals}
           handleOnLoadMore={handleOnLoadMoreUsers}
           idName="username"
-        />
-      </Accordion>
-      <Accordion title="Referral Payments">
-        <TableSearch
-          tableName="payment"
-          onSelectItem={handleOnSelectItem}
-          selected={selected.payment || []}
-          useLoadMore
-          rows={referralRows}
-          showColumnCheck={false}
-          headCells={headCellReferralPayment}
-          handleOnLoadMore={handleOnLoadMoreReferrals}
-          onClickRow={handleOnClickReferral}
-          idName="invitee"
         />
       </Accordion>
       <FloatingMenu open={openFAB} setOpen={setOpenFAB} label="ACTIONS">
