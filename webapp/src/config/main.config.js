02f47ab88b57ee1c10fbb701236827a612c343e2
Adriel Diaz <adriexnet@gmail.com>
2021-08-10 19:15:54 -0600

    feat(webapp): use env variable for contract account

--- webapp/src/config/main.config.js
+++ webapp/src/config/main.config.js
@@ -3,6 +3,7 @@ export const appVersion =
 export const name = process.env.REACT_APP_NAME
 export const title = process.env.REACT_APP_TITLE
 export const blockExplorer = process.env.REACT_APP_BLOCK_EXPLORER_URL
+export const affiliateAccount = process.env.REACT_APP_AFFILIATE_ACCOUNT
 export const logo = process.env.REACT_APP_LOGO
 export const footerLinks = JSON.parse(
   process.env.REACT_APP_FOOTER_LINKS || '[]'
--- webapp/src/utils/affiliate.js
+++ webapp/src/utils/affiliate.js
@@ -1,4 +1,5 @@
 import { eosApi } from './eosapi'
+import { mainConfig } from '../config'
 
 const ROLES = {
   1: 'ADMIN',
@@ -25,8 +26,6 @@ const REFERRAL_STATUS_IDS = {
   PAID: 6
 }
 const GUEST_ROLE = 'NON-AFFILIATED'
-// @todo: use env variable for smart contract name
-const AFFILLIATE_ACCOUNT = 'affiliate'
 
 const getUserRole = async accountName => {
   if (!accountName) {
@@ -35,8 +34,8 @@ const getUserRole = async accountName => {
 
   const { rows } = await eosApi.getTableRows({
     json: true,
-    code: AFFILLIATE_ACCOUNT,
-    scope: AFFILLIATE_ACCOUNT,
+    code: mainConfig.affiliateAccount,
+    scope: mainConfig.affiliateAccount,
     table: 'users',
     lower_bound: accountName,
     upper_bound: accountName
@@ -53,7 +52,7 @@ const addUser = async (admin, users, role = 2) => {
   const transaction = await admin.signTransaction(
     {
       actions: users.map(user => ({
-        account: AFFILLIATE_ACCOUNT,
+        account: mainConfig.affiliateAccount,
         name: 'adduser',
         authorization: [
           {
@@ -80,7 +79,7 @@ const removeUsers = async (admin, users = []) => {
   const transaction = await admin.signTransaction(
     {
       actions: users.map(user => ({
-        account: AFFILLIATE_ACCOUNT,
+        account: mainConfig.affiliateAccount,
         name: 'rmuser',
         authorization: [
           {
@@ -107,7 +106,7 @@ const approveKyc = async (admin, invitee) => {
     {
       actions: [
         {
-          account: AFFILLIATE_ACCOUNT,
+          account: mainConfig.affiliateAccount,
           name: 'setstatus',
           authorization: [
             {
@@ -135,7 +134,7 @@ const payRef = async (admin, invitees = []) => {
   const transaction = await admin.signTransaction(
     {
       actions: invitees.map(invitee => ({
-        account: AFFILLIATE_ACCOUNT,
+        account: mainConfig.affiliateAccount,
         name: 'payref',
         authorization: [
           {
@@ -161,7 +160,7 @@ const rejectRef = async (admin, invitees) => {
   const transaction = await admin.signTransaction(
     {
       actions: invitees.map(invitee => ({
-        account: AFFILLIATE_ACCOUNT,
+        account: mainConfig.affiliateAccount,
         name: 'rejectref',
         authorization: [
           {
@@ -190,8 +189,8 @@ const getUsers = async lowerBound => {
     more: hasMore,
     next_key: cursor
   } = await eosApi.getTableRows({
-    code: AFFILLIATE_ACCOUNT,
-    scope: AFFILLIATE_ACCOUNT,
+    code: mainConfig.affiliateAccount,
+    scope: mainConfig.affiliateAccount,
     table: 'users',
     json: true,
     lower_bound: lowerBound
@@ -210,8 +209,8 @@ const getReferrals = async lowerBound => {
     more: hasMore,
     next_key: cursor
   } = await eosApi.getTableRows({
-    code: AFFILLIATE_ACCOUNT,
-    scope: AFFILLIATE_ACCOUNT,
+    code: mainConfig.affiliateAccount,
+    scope: mainConfig.affiliateAccount,
     table: 'referrals',
     json: true,
     lower_bound: lowerBound
@@ -226,8 +225,8 @@ const getReferrals = async lowerBound => {
 
 const getUser = async account => {
   const { rows } = await eosApi.getTableRows({
-    code: AFFILLIATE_ACCOUNT,
-    scope: AFFILLIATE_ACCOUNT,
+    code: mainConfig.affiliateAccount,
+    scope: mainConfig.affiliateAccount,
     table: 'users',
     json: true,
     lower_bound: account,
