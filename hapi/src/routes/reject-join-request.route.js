70d6c3be900b85614b10927fce6022bb768a7e9c
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2022-01-26 00:54:25 -0600

    feat(hapi): reduce duplicated text for mail templates

--- hapi/src/routes/reject-join-request.route.js
+++ hapi/src/routes/reject-join-request.route.js
@@ -2,33 +2,12 @@ const Joi = require('joi')
 const Boom = require('@hapi/boom')
 
 const { joinRequestService, affiliateService } = require('../services')
-const { mailUtil } = require('../utils')
-const { mailTemplate } = require('../utils/templates')
 
 module.exports = {
   method: 'POST',
   path: '/reject-join-request',
   handler: async ({ payload: { input } }) => {
     try {
-      for (const account of input.accounts) {
-        const joinRequest = await joinRequestService.findByAccount(account)
-        const hasKYC = await affiliateService.checkKyc(input.account)
-
-        if (hasKYC) continue
-
-        try {
-          mailUtil.send({
-            account: account,
-            to: joinRequest.email,
-            subject:
-              'Further action is required to activate your Proton Affiliate account',
-            template: mailTemplate.generateRejectionByKYC
-          })
-        } catch (err) {
-          console.log(err)
-        }
-      }
-
       const transaction = await joinRequestService.removeJoinRequest(
         input.accounts
       )
