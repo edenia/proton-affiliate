d45a820f126d1e20d3e5e4b5f2b5040dbd42c63e
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2022-01-31 16:26:48 -0600

    refactor(hapi): clean code

--- hapi/src/routes/index.js
+++ hapi/src/routes/index.js
@@ -3,13 +3,11 @@ const addReferralRoute = require('./add-referral.route')
 const sendConfirmationRoute = require('./send-confirmation.route')
 const rejectJoinRequestRoute = require('./reject-join-request.route')
 const healthzRoute = require('./healthz.route')
-const testRoute = require('./test.route')
 
 module.exports = [
   addJoinRequest,
   addReferralRoute,
   sendConfirmationRoute,
   rejectJoinRequestRoute,
-  healthzRoute,
-  testRoute
+  healthzRoute
 ]
